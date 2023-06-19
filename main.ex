d98af25d2df3702c25c0a3478872b7ca80757e2b
agentwyz <2012663060@qq.com>
2023-06-19 20:49:30 +0800

    add the main.ex

---
+++ main.ex
@@ -0,0 +1,19 @@
+defmodule Main do
+    
+    
+    list = [1, 2, 3, 4, 5]
+    list1 = [1, 2, 3, 4, 5] ++ [1, 2]
+    hd = hd(list)
+    tl = tail(list)
+
+    map = %{a: 1, b: 2}
+    mapslice = map[:a]
+    map1 = %{map | a: 3}
+
+    defstruct name: "john", age: 27
+
+    def add(a, b) do
+        a + b
+    end
+end
+
